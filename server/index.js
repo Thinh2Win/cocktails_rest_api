5de00a0575673be7345a1c50d9bcef16d4fccf9e
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-09-13 16:36:43 -0700

    Implemented dotenv logic to server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(3000, () => {
-  console.log('server listening on port 3000');
+  console.log(`server listening on port ${process.env.PORT}`);
 });
