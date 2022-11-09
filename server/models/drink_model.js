4199da6b0dccde522d81cffc0b0ebf3f70d2e91e
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-08 16:35:47 -0800

    created model to query db by ingredients

--- server/models/drink_model.js
+++ server/models/drink_model.js
@@ -1,5 +1,5 @@
 const { cocktails } = require('../../server/db/db.js');
-
+const { pascalCase, createExpressions } = require('../helpers/helpers.js');
 const queryRandomDrink = () => {
   return cocktails.aggregate([{$sample: {size: 1}}])
     .then(res => res)
@@ -8,9 +8,7 @@ const queryRandomDrink = () => {
 
 const queryDrinkByName = (param) => {
   // cocktails in db have 1st letters capitol so we need to change our params to match
-  let name = param.split(' ').map(word => {
-    return word.toLowerCase().replace(word[0].toLowerCase(), word[0].toUpperCase());
-  }).join(' ');
+  let name = pascalCase(param);
   // mongodb uses regex as the equivalent to mysql 'LIKE' query
   let regex = new RegExp(String.raw`${name}`);
   return cocktails.find({Name: regex})
@@ -19,21 +17,10 @@ const queryDrinkByName = (param) => {
 };
 
 const queryDrinkByIngredients = (ingredients) => {
-  let items = ingredients.split(',');
-  let query = 'SELECT jsonb_strip_nulls(to_jsonb(recipes)) FROM recipes WHERE ';
-
-  for (let i = 1; i <= 14; i++) {
-    let string = '';
-    string += `(LOWER(stringredient${i}) LIKE LOWER('%${items[0]}%') OR `;
-    for (let j = 1; j < items.length; j++) {
-      string += `LOWER(stringredient${i}) LIKE LOWER('%${items[j]}%') OR `;
-    }
-    string += `LOWER(stringredient${i}) LIKE LOWER('%${items[items.length - 1]}%')) OR `;
-    query += string;
-  }
-  query = query.slice(0, -3);
-  return pool.query(`${query};`)
-    .then(res => res.rows)
+  // ingredients come from route param as string separated by commas
+  let expressions = createExpressions('Ingredients', ingredients);
+  return cocktails.find({$and: expressions})
+    .then(res => res)
     .catch(err => err);
 };
 
