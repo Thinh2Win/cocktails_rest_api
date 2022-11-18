a2774afbcd53f4fa42ac1a06df1ccf254f9a44b9
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-11-17 16:08:29 -0800

    Implemented route for filtering drinks based on ingredients

--- server/routes/drink_routes.js
+++ server/routes/drink_routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const drinksRoutes = express.Router();
-const { getDrinkByName, getRandomDrink, getDrinkByIngredients } = require('../controllers/drink_controllers.js');
+const { getDrinkByName, getRandomDrink, getDrinkByIngredients, filterDrinkByIngredients } = require('../controllers/drink_controllers.js');
 
 drinksRoutes.get('/drink', getRandomDrink);
 
 drinksRoutes.get('/drink/name', getDrinkByName);
 
 drinksRoutes.get('/drink/ingredients', getDrinkByIngredients);
+
+drinksRoutes.get('/drink/filter', filterDrinkByIngredients);
 
 module.exports = drinksRoutes;
