a9b46d62261d82c250315fcdb88302a37fa3c889
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-01 15:42:56 -0800

    Update stress_test.js
    added stress test to filter and search by ingredients routes

--- server/routes/stress_test.js
+++ server/routes/stress_test.js
@@ -2,40 +2,44 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
-export let options = {
-    stages: [
-      { duration: '5s', target: 100 }, // below normal load
-      { duration: '5s', target: 100 },
-      { duration: '10s', target: 200 }, // normal load
-      { duration: '15s', target: 200 },
-      { duration: '25s', target: 300 }, // around the breaking point
-      { duration: '30s', target: 300 },
-      { duration: '25s', target: 400 }, // beyond the breaking point
-      { duration: '25s', target: 400 },
-      { duration: '1m', target: 0 }, // scale down. Recovery stage.
-    ]
-  }
+export const options = {
+  stages: [
+    { duration: '5s', target: 100 }, // below normal load
+    // { duration: '5s', target: 100 },
+    // { duration: '5s', target: 200 }, // normal load
+    // { duration: '5s', target: 200 },
+    // { duration: '10s', target: 300 }, // around the breaking point
+    // { duration: '10s', target: 300 },
+    // { duration: '5s', target: 400 }, // beyond the breaking point
+    // { duration: '5s', target: 400 },
+    // { duration: '5s', target: 0 }, // scale down. Recovery stage.
+  ],
+};
 
-//test for filter and search routes
 // export default function () {
-//   let i = ['lime', 'tequila', 'rum', 'orange', 'pineapple', 'triple sec', 'blue curacao', 'campari', 'sweet vermouth', 'dry vermouth']
-//   http.get(`http://localhost:3000/drink/search?i=${i[Math.floor(Math.random() * 10)]},${i[Math.floor(Math.random() * 10)]},${i[Math.floor(Math.random() * 10)]}`);
-//   sleep(1);
+//   const res = http.get('http://172.18.0.4:3000/drink');
+//   check(res, { 'status was 200': (r) => r.status == 200 });
+//   sleep(0.1);
 // }
 
-// test for drink by name route
-  export default function () {
-    let i = ['negroni', 'red', 'gold', 'royal', 'midori', 'sour']
-    let res = http.get(`http://localhost:3000/drink/name?n=${i[Math.floor(Math.random() * 5)]}`);
-    sleep(1);
-    check (res, {
-        'status code should be 200': res => res.status === 200,
-    });
-  }
+export default function () {
+  const drinks = ['Old%20Fashioned', 'Negroni', 'Daiquiri', 'Dry%20Martini', 'Malibu%20Sunset', 'Piña%20Colada', 'Amaretto%20Sour', 'Mudslide', 'Tequila%20Sunrise', 'AMF', 'Cosmopolitan', 'Long%20Island%20Ice%20Tea', 'Tokyo%20Ice%20Tea', 'Grasshopper', 'Clover%20Club'];
+  const res = http.get(`http://172.18.0.4:3000/drink/name?n=${drinks[Math.floor(Math.random() * drinks.length)]}`);
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
 
-  export function handleSummary(data) {
-    return {
-      "summary.html": htmlReport(data),
-    };
-  }
+// export default function () {
+//   const items = ['Bitters', 'Syrup', 'Flower%20Water', 'Tonic%20Water', 'Ginger%20Beer', 'Gin', 'Tequila', 'Rum', 'Vodka', 'Blue%20Curcao', 'Lime', 'Lemon', 'Orange', 'Apple', 'Pineapple', 'Lychee', 'Mint', 'Whiskey', 'Gin', 'Ginger', 'Sugar', 'blueberry', 'strawberry', 'Triple%20Sec'];
+//   const res = http.get(`http://172.18.0.4:3000/drink/search?i=${items[Math.floor(Math.random() * items.length)]}, ${items[Math.floor(Math.random() * items.length)]}, ${items[Math.floor(Math.random() * items.length)]}, ${items[Math.floor(Math.random() * items.length)]}, ${items[Math.floor(Math.random() * items.length)]}`);
+//   check(res, { 'status was 200': (r) => r.status == 200 });
+//   sleep(0.1);
+// }
+
+// export default function () {
+//   const items = ['Bitters', 'Syrup', 'Flower%20Water', 'Tonic%20Water', 'Ginger%20Beer', 'Gin', 'Tequila', 'Rum', 'Vodka', 'Blue%20Curcao', 'Lime', 'Lemon', 'Orange', 'Apple', 'Pineapple', 'Lychee', 'Mint', 'Whiskey', 'Gin', 'Ginger', 'Sugar', 'blueberry', 'strawberry', 'Triple%20Sec'];
+//   const res = http.get(`http://172.18.0.4:3000/drink/filter?i=${items[Math.floor(Math.random() * items.length)]}, ${items[Math.floor(Math.random() * items.length)]}, ${items[Math.floor(Math.random() * items.length)]}, ${items[Math.floor(Math.random() * items.length)]}, ${items[Math.floor(Math.random() * items.length)]}`);
+//   check(res, { 'status was 200': (r) => r.status == 200 });
+//   sleep(0.1);
+// }
 
