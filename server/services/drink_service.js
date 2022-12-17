ccd212e0333da64a1ac74d211f35513d697fad15
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-16 17:27:42 -0800

    added a services directory to hold db query logic

---
+++ server/services/drink_service.js
@@ -0,0 +1,38 @@
+const cocktails = require('../models/drink_model.js');
+const { createExpressions } = require('../helpers/helpers.js');
+
+const queryRandomDrink = () => {
+  return cocktails.aggregate([{$sample: {size: 1}}]);
+};
+
+const queryDrinkByName = (param) => {
+  let name = param.toLowerCase();
+  // mongodb uses regex as the equivalent to mysql 'LIKE' query
+  let regex = new RegExp(String.raw`${name}`);
+  return cocktails.find({Name: regex})
+    .then(res => res)
+    .catch(err => err);
+};
+
+const queryDrinkByIngredients = (ingredients) => {
+  // ingredients come from route param as string separated by commas
+  // need to create regex expressions for mongodb query
+  let expressions = createExpressions('search', ingredients);
+  return cocktails.find({$and: expressions})
+    .then(res => res)
+    .catch(err => err);
+};
+
+const queryDrinkExcludingIngredients = (ingredients) => {
+  let expressions = createExpressions('filter', ingredients);
+  return cocktails.find({$and: expressions})
+    .then(res => res)
+    .catch(err => err);
+};
+
+module.exports = {
+  queryDrinkByName,
+  queryRandomDrink,
+  queryDrinkByIngredients,
+  queryDrinkExcludingIngredients
+};
