db8923053d5b15ea1620a4ce7b87fb829dd588e9
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-16 17:26:43 -0800

    moved cocktails model to models directory

--- server/db/db.js
+++ server/db/db.js
@@ -8,20 +8,10 @@ const cache = createClient({url: process.env.CACHE_URL});
 cache.connect()
   .catch(err => console.log(err));
 
-const cocktailSchema = new db.Schema({
-  Name: String,
-  Ingredients: String,
-  Garnish: String,
-  Preparation: String
-});
-
 db.connect(process.env.DB_URL)
   .catch(err => console.log(err));
 
-const cocktails = db.model('cocktails', cocktailSchema);
-
 module.exports = {
-  cocktails,
   db,
   cache
 };
