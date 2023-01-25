15e5048827c84ceb5a9e2a37a3004649d5a09771
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-25 15:24:27 -0800

    updated server to run alongside docker containers

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,12 @@
 const db = require('mongoose');
 const { createClient } = require('redis');
 
-require('dotenv').config();
-
-const cache = createClient({url: process.env.CACHE_URL});
+const cache = createClient({url: 'redis://host.docker.internal:6380'});
 
 cache.connect()
   .catch(err => console.log(err));
 
-db.connect(process.env.DB_URL)
+db.connect('mongodb://host.docker.internal:27018/drinks')
   .catch(err => console.log(err));
 
 module.exports = {
