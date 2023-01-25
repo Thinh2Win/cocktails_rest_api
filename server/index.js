4361dbc2af545828d1232cab83a6de9255dc1bcc
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-25 15:27:04 -0800

    added helmet

--- server/index.js
+++ server/index.js
@@ -3,12 +3,11 @@ const helmet = require('helmet');
 
 const app = express();
 const drinksRoutes = require('./routes/drink_routes.js');
-require('dotenv').config();
 
 app.use(helmet());
 app.use(express.json());
 app.use(drinksRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`server listening on port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log('server listening on port 3000');
 });
