4b5bc3dbbf35bd8b930dd6d177a509cbcf029625
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-25 15:29:09 -0800

    updated tests for new schema

--- server/models/drink_model.test.js
+++ server/models/drink_model.test.js
@@ -1,4 +1,4 @@
-const { queryRandomDrink, queryDrinkByName, queryDrinkByIngredients, queryDrinkExcludingIngredients } = require('./drink_model.js');
+const { queryRandomDrink, queryDrinkByName, queryDrinkByIngredients, queryDrinkExcludingIngredients } = require('../services/drink_service.js');
 const { db } = require('../db/db.js');
 
 beforeAll(done => done());
@@ -11,45 +11,49 @@ afterAll(done => {
 test('should return a drink', async () => {
     let data = await queryRandomDrink();
     let keys = Object.keys(data[0]);
-    expect(keys).toContain('Name');
-    expect(keys).toContain('Ingredients');
-    expect(keys).toContain('Preparation');
-    expect(keys).toContain('Garnish');
+    expect(keys).toContain('name');
+    expect(keys).toContain('ingredients');
+    expect(keys).toContain('preparation');
+    expect(keys).toContain('glass');
+    expect(keys).toContain('strength');
+    expect(keys).toContain('description');
+    expect(keys).toContain('img');
+    expect(keys).toContain('profile');
   });
 
 // tests for drink by name query
 test('should return drinks with royal in name', async () => {
   let data = await queryDrinkByName('royal');
   data.forEach(drink => {
-    expect(drink.Name).toContain('royal');
+    expect(drink.name).toContain('royal');
   });
 });
 
 test('should return drinks with negroni in name', async () => {
   let data = await queryDrinkByName('negroni');
   data.forEach(drink => {
-    expect(drink.Name).toContain('negroni');
+    expect(drink.name).toContain('negroni');
   });
 });
 
 test('should return drinks with red in name', async () => {
   let data = await queryDrinkByName('red');
   data.forEach(drink => {
-    expect(drink.Name).toContain('red');
+    expect(drink.name).toContain('red');
   });
 });
 
 test('should return drinks with beach in name', async () => {
   let data = await queryDrinkByName('beach');
   data.forEach(drink => {
-    expect(drink.Name).toContain('beach');
+    expect(drink.name).toContain('beach');
   });
 });
 
 test('should return drinks with coco in name', async () => {
   let data = await queryDrinkByName('coco');
   data.forEach(drink => {
-    expect(drink.Name).toContain('coco');
+    expect(drink.name).toContain('coco');
   });
 });
 
@@ -58,35 +62,35 @@ test('should return drinks with coco in name', async () => {
 test('should return drinks containing lime', async () => {
   let data = await queryDrinkByIngredients('lime');
   data.forEach(drink => {
-    expect(drink.Ingredients).toContain('lime');
+    expect(drink.ingredients).toContain('lime');
   });
 });
 
 test('should return drinks containing lime, ginger, and rum', async () => {
   let data = await queryDrinkByIngredients('lime, ginger, rum');
   data.forEach(drink => {
-    expect(drink.Ingredients).toContain('lime');
-    expect(drink.Ingredients).toContain('ginger');
-    expect(drink.Ingredients).toContain('rum');
+    expect(drink.ingredients).toContain('lime');
+    expect(drink.ingredients).toContain('ginger');
+    expect(drink.ingredients).toContain('rum');
   });
 });
 
 test('should return drinks containing lemon, rum, pineapple, orange', async () => {
   let data = await queryDrinkByIngredients('lemon, rum, pineapple, orange');
   data.forEach(drink => {
-    expect(drink.Ingredients).toContain('lemon');
-    expect(drink.Ingredients).toContain('pineapple');
-    expect(drink.Ingredients).toContain('rum');
-    expect(drink.Ingredients).toContain('orange');
+    expect(drink.ingredients).toContain('lemon');
+    expect(drink.ingredients).toContain('pineapple');
+    expect(drink.ingredients).toContain('rum');
+    expect(drink.ingredients).toContain('orange');
   });
 });
 
 test('should return drinks containing campari, gin, sweet vermouth', async () => {
   let data = await queryDrinkByIngredients('campari, gin, sweet vermouth');
   data.forEach(drink => {
-    expect(drink.Ingredients).toContain('campari');
-    expect(drink.Ingredients).toContain('gin');
-    expect(drink.Ingredients).toContain('sweet vermouth');
+    expect(drink.ingredients).toContain('campari');
+    expect(drink.ingredients).toContain('gin');
+    expect(drink.ingredients).toContain('sweet vermouth');
   });
 });
 
@@ -95,45 +99,45 @@ test('should return drinks containing campari, gin, sweet vermouth', async () =>
 test('should return drinks without tequila', async () => {
   let data = await queryDrinkExcludingIngredients('tequila');
   data.forEach(drink => {
-    expect(drink.Ingredients).not.toContain('tequila');
+    expect(drink.ingredients).not.toContain('tequila');
   });
 });
 
 test('should return drinks without egg whites and syrup', async () => {
   let data = await queryDrinkExcludingIngredients('egg whites, syrup');
   data.forEach(drink => {
-    expect(drink.Ingredients).not.toContain('egg whites');
-    expect(drink.Ingredients).not.toContain('syrup');
+    expect(drink.ingredients).not.toContain('egg whites');
+    expect(drink.ingredients).not.toContain('syrup');
   });
 });
 
 test('should return drinks without gin, lime, whiskey, lemon, pineapple', async () => {
   let data = await queryDrinkExcludingIngredients('gin, lime, whiskey, lemon, pineapple');
   data.forEach(drink => {
-    expect(drink.Ingredients).not.toContain('gin');
-    expect(drink.Ingredients).not.toContain('lime');
-    expect(drink.Ingredients).not.toContain('whiskey');
-    expect(drink.Ingredients).not.toContain('lemon');
-    expect(drink.Ingredients).not.toContain('pineapple');
+    expect(drink.ingredients).not.toContain('gin');
+    expect(drink.ingredients).not.toContain('lime');
+    expect(drink.ingredients).not.toContain('whiskey');
+    expect(drink.ingredients).not.toContain('lemon');
+    expect(drink.ingredients).not.toContain('pineapple');
   });
 });
 
 test('should return drinks without orange, bitters, rum', async () => {
   let data = await queryDrinkExcludingIngredients('orange, bitters, rum');
   data.forEach(drink => {
-    expect(drink.Ingredients).not.toContain('orange');
-    expect(drink.Ingredients).not.toContain('bitters');
-    expect(drink.Ingredients).not.toContain('rum');
+    expect(drink.ingredients).not.toContain('orange');
+    expect(drink.ingredients).not.toContain('bitters');
+    expect(drink.ingredients).not.toContain('rum');
   });
 });
 
 test('should return drinks without campari, bitter, vermouth, apple', async () => {
   let data = await queryDrinkExcludingIngredients('campari, bitter, vermouth, apple');
   data.forEach(drink => {
-    expect(drink.Ingredients).not.toContain('campari');
-    expect(drink.Ingredients).not.toContain('bitters');
-    expect(drink.Ingredients).not.toContain('vermouth');
-    expect(drink.Ingredients).not.toContain('apple');
+    expect(drink.ingredients).not.toContain('campari');
+    expect(drink.ingredients).not.toContain('bitters');
+    expect(drink.ingredients).not.toContain('vermouth');
+    expect(drink.ingredients).not.toContain('apple');
   });
 });
 
