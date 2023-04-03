952a07070c3168b221b51eac9cdb1ac503c81e3a
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-03 15:39:48 -0700

    changed port

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ app.use(helmet());
 app.use(express.json());
 app.use(drinksRoutes);
 
-app.listen(3000, () => {
-  console.log('server listening on port 3000');
+app.listen(3001, () => {
+  console.log('server listening on port 3001');
 });
